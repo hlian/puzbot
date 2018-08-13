58ae318c6a8175356a6d507382503efc5313cd02
Hao Lian <hi@haolian.org>
2018-08-13 15:56:07 -0400

    wow

--- fuse.js
+++ fuse.js
@@ -1,23 +1,24 @@
-const { src, task, exec, context } = require('fuse-box/sparky')
-const { FuseBox, WebIndexPlugin } = require('fuse-box')
+const { src, task, exec, context } = require('fuse-box/sparky');
+const { FuseBox, WebIndexPlugin } = require('fuse-box');
 
-context(class {
-  getConfig() {
-    return FuseBox.init({
-      homeDir: 'src',
-      output: 'dist/$name.js',
-      useTypescriptCompiler: true,
-      experimentalFeatures: true,
-    })
+context(
+  class {
+    getConfig() {
+      return FuseBox.init({
+        homeDir: 'src',
+        output: 'dist/$name.js',
+        useTypescriptCompiler: true,
+        experimentalFeatures: true,
+      });
+    }
   }
-})
+);
 
 task('default', async context => {
-  const fuse = context.getConfig()
-  fuse.bundle('js/back')
-                      .watch('back/**')
-                      .instructions('> [back/index.ts]')
-                      .completed(proc => proc.start())
-  fuse.dev({port: 4444, httpServer: false})
-  fuse.run()
-})
+  const fuse = context.getConfig();
+  fuse
+    .bundle('js/back')
+    .watch('back/**')
+    .instructions('> [back/index.ts]');
+  fuse.run();
+});
--- src/back/index.ts
+++ src/back/index.ts
@@ -1,28 +1,62 @@
-import * as Koa from 'koa'
-import * as KoaStatic from 'koa-static'
-import * as KoaMount from 'koa-mount'
-import * as KoaBody from 'koa-body'
+import * as Koa from 'koa';
+import * as KoaBody from 'koa-body';
+import * as KoaMount from 'koa-mount';
+import * as KoaStatic from 'koa-static';
 
-const app = new Koa()
+import { RTMClient, WebClient } from '@slack/client';
 
-let id = 0
-const db = []
-
-const signUp = (username: string) => {
-  const user = {type: 'user', id: id++, username}
-  db.push(user)
-  return user
+interface Channel {
+  id: string;
+  is_member: boolean;
+  name: string;
 }
 
-app.use(KoaStatic('dist'))
-app.use(KoaBody())
-
-app.use(async ctx => {
-  const body = ctx.request.body
-  if (ctx.url == '/api/sign-up') {
-    console.debug(body, body.username)
-    ctx.body = signUp(body.username)
+const main = async () => {
+  const token = process.env.TOKEN;
+  if (!token) {
+    throw new Error('no TOKEN');
   }
-})
 
-app.listen(3000)
+  const port = process.env.PORT;
+  if (!port) {
+    throw new Error('no PORT');
+  }
+
+  const rtm = new RTMClient(token);
+  rtm.start();
+
+  const web = new WebClient(token);
+  const app = new Koa();
+  app.use(KoaBody());
+  app.use(async ctx => {
+    const body = ctx.request.body;
+    if (!body) {
+      ctx.status = 400;
+      ctx.body = 'no body :(';
+      return;
+    }
+
+    if (body.message) {
+      const { channels: haystack } = ((await web.channels.list()) as any) as {
+        channels: Channel[];
+      };
+
+      const channels = haystack.filter(c => c.is_member && c.name !== 'test');
+      if (channels.length > 0) {
+        rtm.sendMessage(body.message.substring(0, 500), channels[0].id);
+        ctx.body = 'ok';
+      } else {
+        ctx.status = 400;
+        ctx.body = 'no channels :(';
+      }
+    } else {
+      ctx.status = 400;
+      ctx.body = 'unrecognized body :(';
+    }
+  });
+  app.listen(port);
+};
+
+main().catch(e => {
+  process.stderr.write(`main: ${e}\n`);
+});
