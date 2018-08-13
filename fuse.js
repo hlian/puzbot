f0c003c09e1b4244f770e0902db41ee65ca1ab32
Hao Lian <hi@haolian.org>
2018-08-13 14:05:37 -0400

    here we go

---
+++ fuse.js
@@ -0,0 +1,23 @@
+const { src, task, exec, context } = require('fuse-box/sparky')
+const { FuseBox, WebIndexPlugin } = require('fuse-box')
+
+context(class {
+  getConfig() {
+    return FuseBox.init({
+      homeDir: 'src',
+      output: 'dist/$name.js',
+      useTypescriptCompiler: true,
+      experimentalFeatures: true,
+    })
+  }
+})
+
+task('default', async context => {
+  const fuse = context.getConfig()
+  fuse.bundle('js/back')
+                      .watch('back/**')
+                      .instructions('> [back/index.ts]')
+                      .completed(proc => proc.start())
+  fuse.dev({port: 4444, httpServer: false})
+  fuse.run()
+})
